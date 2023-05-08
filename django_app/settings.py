11f876d8038f4a06903225dddbdcdfc706a835dc
Domino123-dot <pl.kubakot@Gmail.com>
2023-05-08 13:22:14 +0200

    test commit

--- django_app/settings.py
+++ django_app/settings.py
@@ -18,8 +18,7 @@ mimetypes.add_type("text/css", ".css", True)
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
