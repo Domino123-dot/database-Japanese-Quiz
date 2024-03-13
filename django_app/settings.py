46b2059112c024ff8d18591601d3ca308a3fcc50
Domino123-dot <pl.kubakot@Gmail.com>
2024-03-13 15:38:17 +0100

    added allowed hosts

--- django_app/settings.py
+++ django_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)&&adh-r((mjh2)2+41zfkvf!quq&&$-2_s+(r=5+qv7+xo3)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
